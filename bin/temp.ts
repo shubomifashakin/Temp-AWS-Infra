841ce79d2646fad503310931e73aa954d38ce883
Fashakin Olashubomi <subomifasakin@outlook.com>
2026-02-13 12:30:35 +0100

    adde github actions stack

--- bin/temp.ts
+++ bin/temp.ts
@@ -3,23 +3,33 @@ import * as cdk from "aws-cdk-lib/core";
 import { TempStack } from "../lib/temp-stack";
 
 import * as dotenv from "dotenv";
+import { GitHubActionsRoleStack } from "../lib/github-actions-role-stack";
 
 dotenv.config();
 
+const notificationEmail = process.env.NOTIFICATION_EMAIL;
+if (!notificationEmail) {
+  throw new Error("NOTIFICATION_EMAIL environment variable must be set");
+}
+
+const githubOrg = process.env.GITHUB_ORG;
+const githubRepo = process.env.GITHUB_REPO;
+
+if (!githubOrg) {
+  throw new Error("GITHUB_ORG environment variable must be set");
+}
+
+if (!githubRepo) {
+  throw new Error("GITHUB_REPO environment variable must be set");
+}
+
 const app = new cdk.App();
+
+new GitHubActionsRoleStack(app, "GitHubActionsStack", {
+  githubOrg,
+  githubRepo,
+});
+
 new TempStack(app, "TempStack", {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
-
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-  notificationEmail: process.env.NOTIFICATION_EMAIL!,
+  notificationEmail,
 });
---
+++ lib/github-actions-role-stack.ts
@@ -0,0 +1,106 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import {
+  Role,
+  WebIdentityPrincipal,
+  ManagedPolicy,
+  PolicyStatement,
+  Effect,
+} from "aws-cdk-lib/aws-iam";
+import { CfnOutput } from "aws-cdk-lib/core";
+
+interface GitHubActionsRoleStackProps extends cdk.StackProps {
+  githubOrg: string;
+  githubRepo: string;
+}
+
+export class GitHubActionsRoleStack extends cdk.Stack {
+  public readonly role: Role;
+
+  constructor(
+    scope: Construct,
+    id: string,
+    props: GitHubActionsRoleStackProps,
+  ) {
+    super(scope, id, props);
+
+    const { githubOrg, githubRepo } = props;
+
+    const githubOidcProviderArn = `arn:aws:iam::${this.account}:oidc-provider/token.actions.githubusercontent.com`;
+
+    this.role = new Role(this, "GitHubActionsDeployRole", {
+      roleName: `github-actions-temp-deploy-role`,
+      description: `Role for GitHub Actions to deploy temp infrastructure`,
+      assumedBy: new WebIdentityPrincipal(githubOidcProviderArn, {
+        StringEquals: {
+          "token.actions.githubusercontent.com:aud": "sts.amazonaws.com",
+        },
+        StringLike: {
+          "token.actions.githubusercontent.com:sub": `repo:${githubOrg}/${githubRepo}:*`,
+        },
+      }),
+      maxSessionDuration: cdk.Duration.hours(1),
+    });
+
+    this.role.addManagedPolicy(
+      ManagedPolicy.fromAwsManagedPolicyName("AWSCloudFormationFullAccess"),
+    );
+
+    this.role.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "s3:*",
+
+          "lambda:*",
+
+          "iam:GetRole",
+          "iam:PassRole",
+          "iam:CreateRole",
+          "iam:DeleteRole",
+          "iam:AttachRolePolicy",
+          "iam:DetachRolePolicy",
+          "iam:PutRolePolicy",
+          "iam:DeleteRolePolicy",
+          "iam:GetRolePolicy",
+          "iam:TagRole",
+          "iam:UntagRole",
+
+          "sqs:*",
+
+          "sns:*",
+
+          "secretsmanager:*",
+
+          "cloudwatch:*",
+          "logs:*",
+
+          "ecr:*",
+
+          "ssm:GetParameter",
+          "ssm:PutParameter",
+        ],
+        resources: ["*"],
+      }),
+    );
+
+    this.role.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: [
+          "iam:CreateUser",
+          "iam:DeleteUser",
+          "iam:GetUser",
+          "iam:TagUser",
+        ],
+        resources: [`arn:aws:iam::${this.account}:user/temp-application-user`],
+      }),
+    );
+
+    new CfnOutput(this, "GitHubActionsRoleArn", {
+      value: this.role.roleArn,
+      description: `GitHub Actions deployment role ARN`,
+      exportName: `GitHubActionsRoleArn`,
+    });
+  }
+}
